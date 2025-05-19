9b9e647a2943309047b11d039e864a4371c042f1
nisar <nisargagn2@gmail.com>
2025-05-18 20:32:45 -0500

    jdbc

--- src/CRUDOperation.java
+++ src/CRUDOperation.java
@@ -3,7 +3,7 @@
 import java.sql.*;
 
 public class CRUDOperation {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) throws  SQLException {
         /*
         import package
         load and register
@@ -16,7 +16,7 @@ public class CRUDOperation {
         String url="jdbc:postgresql://localhost:5434/demo";
         String uname="postgres";
         String pass="Nisarga@913707";
-        String sql="delete from student where sid=5";
+        String sql="insert into student values(5,'nis',80);";
 
         //Class.forName("org.postgresql.Driver");
         Connection con= DriverManager.getConnection(url,uname,pass);
